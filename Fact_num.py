b824cc3f7f5a59a44966dcccb5a919740a5e5a23
rajamohankota <44519160+rajamohankota@users.noreply.github.com>
2020-02-20 14:53:18 +0530

    Update Fact_num.py

--- Fact_num.py
+++ Fact_num.py
@@ -1,8 +1,9 @@
 #Factorial of a number
+import sys
 def fact(n):
     if n == 0:
         return 1
     return n*fact(n-1)
 
-n = int(input("Enter a number to get a factorial: "))
+n = int(sys.argv[1])
 print("Factorial of {} is {} ".format(n,fact(n)))
